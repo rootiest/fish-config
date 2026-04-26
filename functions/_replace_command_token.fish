e9ae94b6cf93618809db92721fa9ecdf3145f222
rootiest <chris@rootiest.com>
2026-04-26 01:48:55 -0400

    fix(keybindings): preserve space after command token on Ctrl+Alt+U

--- functions/_replace_command_token.fish
+++ functions/_replace_command_token.fish
@@ -1,6 +1,6 @@
 function _replace_command_token --description 'Remove first token from commandline and place cursor at start'
     set -l cmd (commandline)
-    set -l rest (string replace -r '^\S+\s*' '' -- $cmd)
+    set -l rest (string replace -r '^\S+' '' -- $cmd)
     commandline -- $rest
     commandline -C 0
 end
