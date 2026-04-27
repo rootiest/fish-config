5c6691c32ce3a8bdb9bdfc3a134fb4b7c001ffd1
rootiest <chris@rootiest.com>
2026-04-27 13:28:14 -0400

    fix: correct sort direction
    Use reverse sorting by time for eza in `ltr` function.
    
    Note: eza sorts oldest-to-newest by default, while lsd requires the
    --reverse flag.
    When the --reverse flag is used in eza the listing is done
    newest-to-oldest which is not the intended direction.

--- functions/ltr.fish
+++ functions/ltr.fish
@@ -3,7 +3,7 @@
 
 function ltr --description 'Reversed time-sorted listing'
     if which eza >/dev/null 2>&1
-        eza --long --all --sort=modified --reverse --icons --hyperlink --color=auto --color-scale=age --color-scale-mode=gradient $argv
+        eza --long --all --sort=modified --icons --hyperlink --color=auto --color-scale=age --color-scale-mode=gradient $argv
     else if which lsd >/dev/null 2>&1
         lsd --long --all --sort=time --reverse --color=auto --hyperlink=always $argv
     else
