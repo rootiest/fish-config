a4d8574f49e86071bef1b748e8a01716faef4448
rootiest <chris@rootiest.com>
2026-04-30 23:13:44 -0400

    refactor: remove obsolete or private functions
    Remove several functions that are either no longer relevant or belong in
    .user-dots as local/machine-specific functions.

--- functions/get-color.fish
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2026 Rootiest
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-function get-color --wraps='/home/rootiest/scripts/hex2rgb.sh "$(wl-colorpicker-plasma)"' --description 'alias get-color=/home/rootiest/scripts/hex2rgb.sh "$(wl-colorpicker-plasma)"'
-  /home/rootiest/scripts/hex2rgb.sh "$(wl-colorpicker-plasma)" $argv
-        
-end
--- functions/nnotes.fish
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2026 Rootiest
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-function nnotes --wraps='cd~/vaults/Rootiest Notes/;nvim' --wraps='cd ~/vaults/Rootiest Notes/;nvim' --wraps='cd ~/vaults/Rootiest\\ Notes/;nvim' --description 'alias nnotes=cd ~/vaults/Rootiest\\ Notes/;nvim'
-  cd ~/vaults/Rootiest\ Notes/;nvim $argv
-        
-end
